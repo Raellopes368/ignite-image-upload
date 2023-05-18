4d2c9c61a128d5d394a5001c090e4d52d3eadab7
Raellopes368 <raellopes368@gmail.com>
2023-05-18 15:15:21 -0300

    Cria modal de visualização de imagens

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,24 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalBody>
+          <Image w="full" h="full" src={imgUrl} alt="Imagem" />
+        </ModalBody>
+        <ModalFooter
+          bg="pGray.800"
+          w="full"
+          h="32px"
+          justifyContent="flex-start"
+        >
+          <Link isExternal href={imgUrl}>
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
